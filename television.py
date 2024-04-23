24215f95b15cb8b6293293ff3cf54010bf6fd53d
beauskers <beau@beattiefamily.us>
2024-04-23 16:37:38 -0500

    Updates

--- television.py
+++ television.py
@@ -4,7 +4,7 @@ class Television:
     min_channel = 0
     max_channel = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Create the Television object with the following attributes:
         status: Boolean, True if the television is on, False otherwise
@@ -13,83 +13,83 @@ class Television:
         channel: Integer, the channel of the television
         previous_volume: Integer, the volume of the television before it was muted
         """
-        self.status = False
-        self.muted = False
-        self.volume = Television.min_volume
-        self.channel = Television.min_channel
-        self.previous_volume = None
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.min_volume
+        self.__channel = Television.min_channel
+        self.__previous_volume = None
 
-    def power(self):
+    def power(self) -> None:
         """
         This method will turn the television on or off
         :return:
         """
-        if not self.status:
-            self.status = True
+        if not self.__status:
+            self.__status = True
         else:
-            self.status = False
+            self.__status = False
 
-    def mute(self):
+    def mute(self) -> None:
         """
         This method will mute or unmute the television
         :return:
         """
-        if self.status:
-            if not self.muted:
-                self.previous_volume = self.volume
-                self.volume = 0
-                self.muted = True
+        if self.__status:
+            if not self.__muted:
+                self.__previous_volume = self.__volume
+                self.__volume = Television.min_volume
+                self.__muted = True
             else:
-                self.volume = self.previous_volume
-                self.muted = False
+                self.__volume = self.__previous_volume
+                self.__muted = False
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         """
         This method will increase the channel of the television
         :return:
         """
-        if self.status:
-            if self.channel < Television.max_channel:
-                self.channel += 1
+        if self.__status:
+            if self.__channel < Television.max_channel:
+                self.__channel += 1
             else:
-                self.channel = Television.min_channel
+                self.__channel = Television.min_channel
 
-    def channel_down(self):
+    def channel_down(self) -> None:
         """
         This method will decrease the channel of the television
         :return:
         """
-        if self.status:
-            if self.channel > Television.min_channel:
-                self.channel -= 1
+        if self.__status:
+            if self.__channel > Television.min_channel:
+                self.__channel -= 1
             else:
-                self.channel = Television.max_channel
+                self.__channel = Television.max_channel
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         """
         This method will increase the volume of the television
         :return:
         """
-        if self.status:
-            if self.muted:
+        if self.__status:
+            if self.__muted:
                 self.mute()
-            if self.volume < Television.max_volume:
-                self.volume += 1
+            if self.__volume < Television.max_volume:
+                self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         """
         This method will decrease the volume of the television
         :return:
         """
-        if self.status:
-            if self.muted:
+        if self.__status:
+            if self.__muted:
                 self.mute()
-            if self.volume > Television.min_volume:
-                self.volume -= 1
+            if self.__volume > Television.min_volume:
+                self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         This method will return a string for the Television object
         :return:
         """
-        return f'Power = [{self.status}], Channel = [{self.channel}], Volume = [{self.volume}], '
+        return f'Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{self.__volume}], '
--- test_television.py
+++ test_television.py
@@ -5,52 +5,29 @@ tv = Television()
 
 
 def test_init():
-    assert tv.status == False
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
     assert str(tv) == 'Power = [False], Channel = [0], Volume = [0], '
 
 
 def test_power():
-    assert tv.status == False
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
-
+    assert str(tv) == 'Power = [False], Channel = [0], Volume = [0], '
     tv.power()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [0], '
     tv.power()
 
 
 def test_mute():
     tv.mute()
-    assert tv.status == False
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [False], Channel = [0], Volume = [0], '
 
     tv.mute()
-    assert tv.status == False
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [False], Channel = [0], Volume = [0], '
 
     tv.power()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [0], '
 
     tv.volume_up()
     tv.mute()
-    assert tv.status == True
-    assert tv.muted == True
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [0], '
     tv.mute()
     tv.volume_down()
     tv.power()
@@ -58,97 +35,58 @@ def test_mute():
 
 def test_channel_up():
     tv.channel_up()
-    assert tv.status == False
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [False], Channel = [0], Volume = [0], '
 
     tv.power()
     tv.channel_up()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel + 1
+    assert str(tv) == 'Power = [True], Channel = [1], Volume = [0], '
 
     tv.channel_up()
     tv.channel_up()
     tv.channel_up()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [0], '
     tv.power()
 
 
 def test_channel_down():
     tv.channel_down()
-    assert tv.status == False
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [False], Channel = [0], Volume = [0], '
 
     tv.power()
     tv.channel_down()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.max_channel
+    assert str(tv) == 'Power = [True], Channel = [3], Volume = [0], '
     tv.channel_up()
     tv.power()
 
 
 def test_volume_up():
     tv.volume_up()
-    assert tv.status == False
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [False], Channel = [0], Volume = [0], '
 
     tv.power()
     tv.volume_up()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume + 1
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [1], '
 
     tv.mute()
     tv.volume_up()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume + 2
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [2], '
 
     tv.volume_up()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.max_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [2], '
     tv.power()
 
 
 def test_volume_down():
     tv.volume_down()
-    assert tv.status == False
-    assert tv.muted == False
-    assert tv.volume == Television.max_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [False], Channel = [0], Volume = [2], '
 
     tv.power()
     tv.volume_down()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.max_volume - 1
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [1], '
 
     tv.mute()
     tv.volume_down()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [0], '
 
     tv.volume_down()
-    assert tv.status == True
-    assert tv.muted == False
-    assert tv.volume == Television.min_volume
-    assert tv.channel == Television.min_channel
+    assert str(tv) == 'Power = [True], Channel = [0], Volume = [0], '
